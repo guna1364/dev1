f3718e93bdae6dd154ea1c1f42a2bca379493954
Y Guna Sekhar <gunasekhary@herbalife.com>
2018-12-02 12:52:56 +0530

    newfiles

---
+++ test.sh
@@ -0,0 +1,6 @@
+echo "------------WELCOME-------------------------"
+echo " You are living in the sate of : $states 
+       With Capital of :$Capital 
+
+       Thanks for Visiting 
+       ------------------------------------"
